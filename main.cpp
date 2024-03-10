730495f3b126c5887bd68d4f9bc89610c1cb6a04
smorf1koo <gepetrov@edu.hse.ru>
2024-03-11 02:49:01 +0300

    Task2

--- main.cpp
+++ main.cpp
@@ -2,44 +2,77 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <list>
+
 //перегрузил оператор вывода для вектора любого типа данных
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec){
-    auto print = [&os](const T& value) {os << value << ' ';};
-    for_each(vec.begin(), vec.end(), print);
+template<typename Container>
+std::ostream& printContainer(std::ostream& os, const Container& cont) {
+    auto print = [&os](const typename Container::value_type& value) {
+        os << value << ' ';
+    };
+    for_each(cont.begin(), cont.end(), print);
     os << '\n';
     return os;
 }
-int main(){
 
-    //TASK 1
-    std::cout << "TASK 1\n";
-    std::vector <int> vec;
-    unsigned int n;
-    std::cout << "Enter vector size: \n";
-    std::cin >> n;
-    int value;
-    while (vec.size() < n){
-        std::cin >> value;
-        vec.push_back(value);
-    }
+// Перегрузка оператора вывода для вектора
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    return printContainer(os, vec);
+}
+
+// Перегрузка оператора вывода для списка
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::list<T>& list) {
+    return printContainer(os, list);
+}
+
+int main(){
+//
+//    //TASK 1
+//    std::cout << "TASK 1\n";
+//    std::list <int> list;
+//    unsigned int n;
+//    std::cout << "Enter List size: \n";
+//    std::cin >> n;
+//    int value;
+//    while (list.size() < n){
+//        std::cin >> value;
+//        list.push_back(value);
+//    }
+//    std::cout << "Entered List: " << list;
+//
+//    //проверим, есть ли в векторе число, кратное 5
+//    if (std::find_if(list.begin(), list.end(), [](int i) { return i % 5 == 0; }) != std::end(list)) {
+//        std::cout << "List includes a number divisible by 5\n";
+//    } else {
+//        std::cout << "List doesn't include a number divisible by 5\n";
+//    }
+//
+//    // посчитаем отрицательные символы
+//    int count = std::count_if(list.begin(), list.end(), [](int i){ return i < 0; });
+//    std::cout << "Negative values: " << count << "\n";
+//
+//    //посчитаем сумму элементов
+//    int sum = std::accumulate(list.begin(), list.end(), 0);
+//    std:: cout << "Sum of vector elements: " << sum << '\n';
+//    list.clear(); //освобождаем память
+
+    //TASK2
+    std::cout << "TASK 2\n";
+    std::vector <int> vec = {1, 5, 6, -42, 5, 52};
     std::cout << "Entered vector: " << vec;
 
-    //проверим, есть ли в векторе число, кратное 5
-    if (std::find_if(vec.begin(), vec.end(), [](int i) { return i % 5 == 0; }) != std::end(vec)) {
-        std::cout << "Vector includes a number divisible by 5\n";
-    } else {
-        std::cout << "Vector doesn't include a number divisible by 5\n";
-    }
+    //добавим значения в конец и после 5 элемента
+    std::insert_iterator <std::vector <int>> inserter(vec, 5+vec.begin());
+    *inserter = 10;
+    std::back_inserter(vec) = -1;
+    std::cout << "Vector with new elements: " << vec;
 
-    // отсортируем по убыванию
-    std::cout << "Sorting...\n";
-    std::sort(vec.begin(), vec.end(), [](int a, int b){return a < b;});
-    std::cout << "Sorted vector: " << vec;
-
-    //посчитаем сумму элементов
-    int sum = std::accumulate(vec.begin(), vec.end(), 0);
-    std:: cout << "Sum of vector elements: " << sum << '\n';
-
+    //переместимся итератором на желаемую позицию и удалим все элменты до нее
+    auto it = vec.begin();
+    std::advance(it, 2);
+    vec.erase(vec.begin(), it);
+    std::cout << "Vector without 1-2 elements: " << vec;
     return 0;
 }
